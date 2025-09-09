da36b05af40bf8f60fbea88ab84f86a7f506503f
Anni Herttuainen <bhq712@myy.haaga-helia.fi>
2025-09-09 16:15:23 +0300

    Lisätty booklist.html ja päivitetty BookController

--- src/main/java/hh/backend/bookstore/web/BookController.java
+++ src/main/java/hh/backend/bookstore/web/BookController.java
@@ -3,13 +3,26 @@ package hh.backend.bookstore.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import hh.backend.bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
+
+    BookRepository bookRepository;
+
+	BookController(BookRepository bookRepository){
+		this.bookRepository = bookRepository;
+	}
+
+
     @GetMapping("/bookstore")
     public String WelcomeToTheSite(Model model){
         return "etusivu"; //etusivu.html
     }
 
-
+    @GetMapping("/booklist")
+    public String listAllBooks(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "booklist"; //booklist.html
+    }
 }
