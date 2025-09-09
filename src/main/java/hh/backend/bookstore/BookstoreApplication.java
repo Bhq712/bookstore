fcd03d33601f26f72cd11c6afa9ab4a912731270
Anni Herttuainen <bhq712@myy.haaga-helia.fi>
2025-09-09 16:00:09 +0300

    muutokset

--- src/main/java/hh/backend/bookstore/BookstoreApplication.java
+++ src/main/java/hh/backend/bookstore/BookstoreApplication.java
@@ -1,13 +1,37 @@
 package hh.backend.bookstore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import hh.backend.bookstore.domain.BookRepository;
+import hh.backend.bookstore.domain.Book;
+
 
 @SpringBootApplication
 public class BookstoreApplication {
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner BoostoreDemo(BookRepository bookRepository) {
+		return (args) -> {
+			log.info("save a couple of books");
+			bookRepository.save(new Book("Orvot katseet", "Janice Muller", 2020, "1234567890", 13.45));
+			bookRepository.save(new Book("Kaukainen tulevaisuus", "Adam Kenner", 2023, "2234567890", 19.99));
+			bookRepository.save(new Book("Kultainen tie", "Talia Smith", 1999, "3234567890", 24.99));
+			bookRepository.save(new Book("Tulikärpänen", "Joanna Geller", 2012, "4234567890", 21.56));
+
+			log.info("fetch all books");
+			for (Book book : bookRepository.findAll()) {
+				log.info(book.toString());
+			}
+		};
+	}
 }
--- src/main/java/hh/backend/bookstore/domain/Book.java
+++ src/main/java/hh/backend/bookstore/domain/Book.java
@@ -1,16 +1,95 @@
 package hh.backend.bookstore.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+
 public class Book {
 
-    //attribuutit
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
+    //attribuutit
+    private Long id;
     private String title;
     private String author;
     private int publicationYear;
     private String isbn;
     private double price;
 
+    protected Book() {}
+
+    //konstruktorit
+    public Book(String title, String author, int publicationYear, String isbn, double price) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+        this.isbn = isbn;
+        this.price = price;
+    }
+
+    //getit ja setit
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    //toString
+    @Override
+    public String toString() {
+        return "Book [id=" + id + ", title=" + title + ", author=" + author + ", publicationYear=" + publicationYear
+                + ", isbn=" + isbn + ", price=" + price + "]";
+    }
+
+    
   
+    
 
     
 
